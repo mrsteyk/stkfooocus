1023281a946c8cbcc0476f6ae5074158c18c3acf
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2023-08-17 17:24:51 +0500

    init

---
+++ main.py
@@ -0,0 +1,241 @@
+import argparse
+import sys
+import os
+import time
+
+import gradio as gr
+
+import shared
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "ComfyUI"))
+
+import modules.html  # noqa
+import modules.async_worker as worker  # noqa
+import modules.path  # noqa
+from modules.sdxl_styles import style_keys, aspect_ratios  # noqa
+
+
+def generate_clicked(*args):
+    yield gr.update(interactive=False), gr.update(
+        visible=True,
+        value=modules.html.make_progress_html(1, "Processing text encoding ..."),
+    ), gr.update(visible=True, value=None), gr.update(visible=False)
+
+    worker.buffer.append(list(args))
+    finished = False
+
+    while not finished:
+        time.sleep(0.01)
+        if len(worker.outputs) > 0:
+            flag, product = worker.outputs.pop(0)
+            if flag == "preview":
+                percentage, title, image = product
+                yield gr.update(interactive=False), gr.update(
+                    visible=True,
+                    value=modules.html.make_progress_html(percentage, title),
+                ), gr.update(
+                    visible=True, value=image
+                ) if image is not None else gr.update(), gr.update(
+                    visible=False
+                )
+            if flag == "results":
+                yield gr.update(interactive=True), gr.update(visible=False), gr.update(
+                    visible=False
+                ), gr.update(visible=True, value=product)
+                finished = True
+    return
+
+
+if __name__ == "__main__":
+    print("Starting...")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=None, help="Set the listen port.")
+    parser.add_argument(
+        "--share", action="store_true", help="Set whether to share on Gradio."
+    )
+    parser.add_argument(
+        "--listen",
+        type=str,
+        default=None,
+        metavar="IP",
+        nargs="?",
+        const="0.0.0.0",
+        help="Set the listen interface.",
+    )
+    args = parser.parse_args()
+    # Don't use xformers (useless in this context?)
+    sys.argv += ["--use-pytorch-cross-attention"]
+
+    shared.gradio_root = gr.Blocks(title="Fooocus STK", css=modules.html.css).queue()
+    with shared.gradio_root:
+        with gr.Row():
+            with gr.Column():
+                progress_window = gr.Image(
+                    label="Preview", show_label=True, height=640, visible=False
+                )
+                progress_html = gr.HTML(
+                    value=modules.html.make_progress_html(32, "Progress 32%"),
+                    visible=False,
+                    elem_id="progress-bar",
+                    elem_classes="progress-bar",
+                )
+                gallery = gr.Gallery(
+                    label="Gallery",
+                    show_label=False,
+                    object_fit="contain",
+                    height=720,
+                    visible=True,
+                )
+                with gr.Row(elem_classes="type_row"):
+                    with gr.Column(scale=0.85):
+                        prompt = gr.Textbox(
+                            show_label=False,
+                            placeholder="Type prompt here.",
+                            container=False,
+                            autofocus=True,
+                            elem_classes="type_row",
+                            lines=1024,
+                        )
+                    with gr.Column(scale=0.15, min_width=0):
+                        run_button = gr.Button(
+                            label="Generate", value="Generate", elem_classes="type_row"
+                        )
+                with gr.Row():
+                    advanced_checkbox = gr.Checkbox(
+                        label="Advanced", value=False, container=False
+                    )
+            with gr.Column(scale=0.5, visible=False) as right_col:
+                with gr.Tab(label="Setting"):
+                    performance_selction = gr.Radio(
+                        label="Performance", choices=["Speed", "Quality"], value="Speed"
+                    )
+                    aspect_ratios_selction = gr.Radio(
+                        label="Aspect Ratios (width × height)",
+                        choices=list(aspect_ratios.keys()),
+                        value="1152×896",
+                    )
+                    image_number = gr.Slider(
+                        label="Image Number", minimum=1, maximum=32, step=1, value=2
+                    )
+                    image_seed = gr.Number(label="Random Seed", value=-1, precision=0)
+                    negative_prompt = gr.Textbox(
+                        label="Negative Prompt",
+                        show_label=True,
+                        placeholder="Type prompt here.",
+                    )
+                with gr.Tab(label="Style"):
+                    style_selction = gr.Radio(
+                        show_label=False,
+                        container=True,
+                        choices=style_keys,
+                        value="cinematic-default",
+                    )
+                with gr.Tab(label="Advanced"):
+                    with gr.Row():
+                        base_model = gr.Dropdown(
+                            label="SDXL Base Model",
+                            choices=modules.path.model_filenames,
+                            value=modules.path.default_base_model_name,
+                            show_label=True,
+                        )
+                        refiner_model = gr.Dropdown(
+                            label="SDXL Refiner",
+                            choices=["None"] + modules.path.model_filenames,
+                            value=modules.path.default_refiner_model_name,
+                            show_label=True,
+                        )
+                    with gr.Accordion(label="LoRAs", open=True):
+                        lora_ctrls = []
+                        for i in range(5):
+                            with gr.Row():
+                                lora_model = gr.Dropdown(
+                                    label=f"SDXL LoRA {i+1}",
+                                    choices=["None"] + modules.path.lora_filenames,
+                                    value=modules.path.default_lora_name
+                                    if i == 0
+                                    else "None",
+                                )
+                                lora_weight = gr.Slider(
+                                    label="Weight",
+                                    minimum=-2,
+                                    maximum=2,
+                                    step=0.01,
+                                    value=modules.path.default_lora_weight,
+                                )
+                                lora_ctrls += [lora_model, lora_weight]
+                    with gr.Row():
+                        model_refresh = gr.Button(
+                            label="Refresh",
+                            value="\U0001f504 Refresh All Files",
+                            variant="secondary",
+                            elem_classes="refresh_button",
+                        )
+                    with gr.Accordion(label="Advanced", open=False):
+                        sharpness = gr.Slider(
+                            label="Sampling Sharpness",
+                            minimum=0.0,
+                            maximum=40.0,
+                            step=0.01,
+                            value=2.0,
+                        )
+                        sampler_selection = gr.Dropdown(
+                            label="Sampler",
+                            choices=[
+                                "dpmpp_2m_sde_gpu",
+                                "dpmpp_2m_sde",
+                                "dpmpp_3m_sde_gpu",
+                                "dpmpp_3m_sde",
+                            ],
+                            value="dpmpp_2m_sde_gpu",
+                        )
+                        gr.HTML(
+                            '<a href="https://github.com/lllyasviel/Fooocus/discussions/117">\U0001F4D4 Document</a>'
+                        )
+
+                    def model_refresh_clicked():
+                        modules.path.update_all_model_names()
+                        results = []
+                        results += [
+                            gr.update(choices=modules.path.model_filenames),
+                            gr.update(choices=["None"] + modules.path.model_filenames),
+                        ]
+                        for i in range(5):
+                            results += [
+                                gr.update(
+                                    choices=["None"] + modules.path.lora_filenames
+                                ),
+                                gr.update(),
+                            ]
+                        return results
+
+                    model_refresh.click(
+                        model_refresh_clicked,
+                        [],
+                        [base_model, refiner_model] + lora_ctrls,
+                    )
+
+            advanced_checkbox.change(
+                lambda x: gr.update(visible=x), advanced_checkbox, right_col
+            )
+            ctrls = [
+                prompt,
+                negative_prompt,
+                style_selction,
+                performance_selction,
+                aspect_ratios_selction,
+                image_number,
+                image_seed,
+                sharpness,
+                sampler_selection,
+            ]
+            ctrls += [base_model, refiner_model] + lora_ctrls
+            run_button.click(
+                fn=generate_clicked,
+                inputs=ctrls,
+                outputs=[run_button, progress_html, progress_window, gallery],
+            )
+
+    shared.gradio_root.launch(
+        inbrowser=True, server_name=args.listen, server_port=args.port, share=args.share
+    )
---
+++ modules/async_worker.py
@@ -0,0 +1,140 @@
+import threading
+
+
+buffer = []
+outputs = []
+
+
+def worker():
+    global buffer, outputs
+
+    import time
+    import shared
+    import random
+    import modules.default_pipeline as pipeline
+    import modules.path
+    import modules.patch
+
+    from modules.sdxl_styles import apply_style, aspect_ratios
+
+    # from modules.private_logger import log
+
+    try:
+        async_gradio_app = shared.gradio_root
+        flag = f"""App started successful. Use the app with {str(async_gradio_app.local_url)} or {str(async_gradio_app.server_name)}:{str(async_gradio_app.server_port)}"""
+        if async_gradio_app.share:
+            flag += f""" or {async_gradio_app.share_url}"""
+        print(flag)
+    except Exception as e:
+        print(e)
+
+    def handler(task):
+        (
+            prompt,
+            negative_prompt,
+            style_selction,
+            performance_selction,
+            aspect_ratios_selction,
+            image_number,
+            image_seed,
+            sharpness,
+            sampler_selection,
+            base_model_name,
+            refiner_model_name,
+            l1,
+            w1,
+            l2,
+            w2,
+            l3,
+            w3,
+            l4,
+            w4,
+            l5,
+            w5,
+        ) = task
+
+        loras = [(l1, w1), (l2, w2), (l3, w3), (l4, w4), (l5, w5)]
+
+        modules.patch.sharpness = sharpness
+
+        # pipeline.refresh_base_model(base_model_name)
+        pipeline.refresh_refiner_model(refiner_model_name)
+        pipeline.refresh_base_with_loras(base_model_name, loras)
+        pipeline.clean_prompt_cond_caches()
+
+        p_txt, n_txt = apply_style(style_selction, prompt, negative_prompt)
+
+        if performance_selction == "Speed":
+            steps = 30
+            switch = 20
+        else:
+            steps = 60
+            switch = 40
+
+        width, height = aspect_ratios[aspect_ratios_selction]
+
+        results = []
+        seed = image_seed
+        if not isinstance(seed, int) or seed < 0 or seed > 1024 * 1024 * 1024:
+            seed = random.randint(1, 1024 * 1024 * 1024)
+
+        all_steps = steps * image_number
+
+        def callback(step, x0, x, total_steps, y):
+            done_steps = i * steps + step
+            outputs.append(
+                [
+                    "preview",
+                    (
+                        int(100.0 * float(done_steps) / float(all_steps)),
+                        f"Step {step}/{total_steps} in the {i}-th Sampling",
+                        y,
+                    ),
+                ]
+            )
+
+        for i in range(image_number):
+            imgs = pipeline.process(
+                p_txt,
+                n_txt,
+                steps,
+                switch,
+                width,
+                height,
+                seed,
+                sampler_selection,
+                callback=callback,
+            )
+
+            for x in imgs:
+                d = [
+                    ("Prompt", prompt),
+                    ("Negative Prompt", negative_prompt),
+                    ("Style", style_selction),
+                    ("Performance", performance_selction),
+                    ("Resolution", str((width, height))),
+                    ("Sharpness", sharpness),
+                    ("Base Model", base_model_name),
+                    ("Refiner Model", refiner_model_name),
+                    ("Seed", seed),
+                ]
+                for n, w in loras:
+                    if n != "None":
+                        d.append((f"LoRA [{n}] weight", w))
+                # log(x, d)
+
+            seed += 1
+            results += imgs
+
+        outputs.append(["results", results])
+        return
+
+    while True:
+        time.sleep(0.01)
+        if len(buffer) > 0:
+            task = buffer.pop(0)
+            handler(task)
+    pass
+
+
+threading.Thread(target=worker, daemon=True).start()
